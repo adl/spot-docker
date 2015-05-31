f6935f16d715eb17700b0635ccba988735068664
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2015-05-31 14:54:53 +0200

    debuild: do not set PATH, instead pass --prepand-path to debuild

--- debuild/build-spot.sh
+++ debuild/build-spot.sh
@@ -11,13 +11,15 @@ set -x
 br=$1
 shift
 rm -rf spot
-export PATH=$PATH:/usr/local/bin
 git clone https://gitlab.lrde.epita.fr/spot/spot.git -b "$br" --single-branch
 cd spot
 autoreconf -vfi
 ./configure PYTHON=/usr/bin/python3 --disable-static
 make "$@"
-make deb "$@" DEBUILDFLAGS="$*"
+# Add /usr/local/bin so that the test-suite uses the locally installed
+# third-party tools.  The build would work even without modifying
+# PATH, but in that case it would skip several tests.
+make deb "$@" DEBUILDFLAGS="--prepend-path=/usr/local/bin $*"
 
 test -d ../result &&
     mv -f *.deb ../result &&
